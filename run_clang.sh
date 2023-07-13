19f19b372545c602ad36ee930b0120163bd07ddf
FloyzenCode <vasyplay323@gmail.com>
2023-07-13 15:18:39 +0400

    Add file

---
+++ run_clang.sh
@@ -0,0 +1,3 @@
+toilet --gay CLANG++
+clang++ -std=c++20 -o run_file_compile main.cpp
+./run_file_compile
---
+++ run_gcc.sh
@@ -0,0 +1,3 @@
+echo "Start compiler"
+g++ -std=c++20 -o run_file_compile main.cpp
+./run_file_compile
