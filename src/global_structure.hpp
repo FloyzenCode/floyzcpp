9e37bc1709e0b9923e24d985025636f8807bb240
FloyzenCode <vasyplay323@gmail.com>
2023-07-21 11:25:10 +0300

    Update code

--- src/global_structure.hpp
+++ src/global_structure.hpp
@@ -12,6 +12,8 @@
 #include <array>
 #include <chrono>
 #include <thread>
+#include <filesystem>
+#include <fstream>
 
 namespace fcpp
 {
@@ -44,40 +46,143 @@ namespace fcpp
         fcpp::help();
     }
 
+    bool hasSpaces(const std::string &str)
+    {
+        return str.find(' ') != std::string::npos;
+    }
+
     // default config
-    int createDefault()
+    int createDefault() // fstream / std::filesystem / системные вызовы / что угодно иное
     {
         std::string projectName;
         std::cout << "project name: ";
         std::getline(std::cin, projectName);
-
+        if (hasSpaces(projectName))
+        {
+            fcpp::error();
+            return 1;
+        }
         std::cout << "Create " << projectName << " project" << std::endl
                   << "Wait second...";
 
-        system(("mkdir " + projectName).c_str()); // create project folder
+        // create parent folder
+        std::filesystem::create_directory(projectName);
 
+        // create structure project
+        std::filesystem::create_directory(projectName + "/include");
+        std::cout << "Create include..." << std::endl;
+        std::ofstream helloWorldFile(projectName + "/include/helloWorld.hpp");
+        helloWorldFile << "#include <iostream>\n"
+                       << "\n"
+                       << "void helloWorld()\n"
+                       << "{\n"
+                       << "    std::cout << \"Hello World!\" << std::endl;\n"
+                       << "}\n";
+        helloWorldFile.close();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/src");
+
+        std::ofstream mainFile(projectName + "/src/main.cpp");
+        mainFile << "#include \"../include/helloWorld.hpp\"\n"
+                 << "\n"
+                 << "int main()\n"
+                 << "{\n"
+                 << "    helloWorld();\n"
+                 << "    return 0;\n"
+                 << "}";
+        mainFile.close();
+
+        std::cout << "Create source..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/docs");
+        std::cout << "Create docs..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/lib");
+        std::cout << "Create lib..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/test");
+        std::cout << "Create test..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/scripts");
+        std::cout << "Create scripts..." << std::endl;
+        std::ofstream CMakeList(projectName + "/CMakeLists.txt");
+        CMakeList << "cmake_minimum_required(VERSION 3.20)";
+        CMakeList.close();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        std::cout << "\nFolder - done!\n";
-
-        system(("cd " + projectName).c_str());
-        system(("echo \"SSS\" >> ads.txt"));
+        std::cout << "\nGenerate project done!\n";
         return 0;
     }
 
     // library config
-    int createLibrary()
+    int createLibrary() // fstream / std::filesystem / системные вызовы / что угодно иное
     {
         std::string projectName;
         std::cout << "project name: ";
         std::getline(std::cin, projectName);
-
+        if (hasSpaces(projectName))
+        {
+            fcpp::error();
+            return 1;
+        }
         std::cout << "Create " << projectName << " project" << std::endl
                   << "Wait second...";
 
-        system(("mkdir " + projectName).c_str());
+        // create parent folder
+        std::filesystem::create_directory(projectName);
 
+        // create structure project
+        std::filesystem::create_directory(projectName + "/include");
+        std::cout << "Create include..." << std::endl;
+        std::ofstream module1(projectName + "/include/helloWorld.hpp");
+        module1 << "#include <iostream>\n"
+                << "\n"
+                << "void helloWorld()\n"
+                << "{\n"
+                << "    std::cout << \"Hello World!\" << std::endl;\n"
+                << "}\n";
+        module1.close();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/src");
+
+        std::ofstream mainFile(projectName + "/main.cpp");
+        mainFile << "#include \"../include/helloWorld.hpp\"\n"
+                 << "\n"
+                 << "int main()\n"
+                 << "{\n"
+                 << "    helloWorld();\n"
+                 << "    return 0;\n"
+                 << "}";
+        mainFile.close();
+
+        std::cout << "Create source..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/docs");
+        std::cout << "Create docs..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/lib");
+        std::ofstream file(projectName + "/lib/file.hpp");
+        file << "#include <iostream>\n\n"
+             << "int foo() {\n"
+             << "    std::cout << \"Example file\";\n"
+             << "    return 0;\n"
+             << "}";
+        std::cout << "Create lib..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/test");
+        std::cout << "Create test..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(800));
+        std::filesystem::create_directory(projectName + "/scripts");
+        std::cout << "Create scripts..." << std::endl;
+        std::ofstream CMakeList(projectName + "/CMakeLists.txt");
+        CMakeList << "cmake_minimum_required(VERSION 3.20)";
+        CMakeList.close();
+        std::ofstream readme(projectName + "/README.md");
+        readme << "# " << projectName << std::endl
+               << "Project description...";
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        std::cout << "\nFolder - done!\n";
+        std::cout << "\nGenerate project done!\n";
         return 0;
     }
 
