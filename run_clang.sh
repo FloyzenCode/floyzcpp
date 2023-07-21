8234098f94d1fe791d265abea4740721b37a0e57
FloyzenCode <vasyplay323@gmail.com>
2023-07-21 21:56:16 +0300

    Deleted

--- run_clang.sh
+++
@@ -1,4 +0,0 @@
-toilet --gay CLANG++
-cd src
-clang++ -std=c++20 -o run_file_compile main.cpp
-echo "run ./floyzcpp <args>\n<args> = create"
--- run_gcc.sh
+++
@@ -1,7 +0,0 @@
-echo "Start compiler"
-cd src
-g++ -std=c++20 -o run_file_compile main.cpp
-echo "Run:"
-echo "cd src"
-echo "./floyzcpp <args>"
-echo "<args> = create"
